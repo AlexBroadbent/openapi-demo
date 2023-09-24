c5da32aa63f17d3c39ed01cbad00919b79202c7e
Alexander Broadbent <alexbroadbent14@gmail.com>
2023-09-24 18:15:26 +0100

    Update bundled path

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -7,7 +7,7 @@ import { cityController } from "../module/city/controller"
 import { routeController } from "../module/route/controller"
 import securityHandlers from "./security-handlers"
 
-const specification = path.join(__dirname, "../config/openapi.bundle.json")
+const specification = path.join(__dirname, "../config/openapi.bundle.yml")
 
 const service = {
   ...healthController,
--- src/types/generated/openapi-schema.ts
+++ src/types/generated/openapi-schema.ts
@@ -3,73 +3,74 @@
  * Do not make direct changes to the file.
  */
 
+
 export interface paths {
   "/city/{id}": {
     /**
      * Get City
      * @description Get a city given an ID
      */
-    get: operations["getCity"]
-  }
+    get: operations["getCity"];
+  };
   "/route": {
     /**
      * Get a Route
      * @description Get a route between two cities
      */
-    get: operations["getRoute"]
-  }
+    get: operations["getRoute"];
+  };
   "/_health": {
     /**
      * Get Health Check Status
      * @description Returns the health check for the service
      */
-    get: operations["getHealthCheck"]
-  }
+    get: operations["getHealthCheck"];
+  };
 }
 
-export type webhooks = Record<string, never>
+export type webhooks = Record<string, never>;
 
 export interface components {
   schemas: {
     /** Health Check Result */
     HealthCheckResult: {
-      ok: boolean
-    }
+      ok: boolean;
+    };
     /** Error Model */
     ErrorModel: {
-      status: number
-      message: string
-    }
+      status: number;
+      message: string;
+    };
     /** City */
     City: {
       /** @description City Identifier */
-      id: string
+      id: string;
       /** @description Name of the city */
-      name: string
+      name: string;
       /** @description The country which the city is in */
-      country: string
-    }
+      country: string;
+    };
     /**
      * Route
      * @description A route between two cities (`from` and `to`), with a distance of miles
      */
     Route: {
       /** @description A city identifier */
-      from: string
+      from: string;
       /** @description A city identifier */
-      to: string
+      to: string;
       /**
        * Format: int32
        * @description The distance in miles between the two cities
        */
-      miles: number
-    }
-  }
+      miles: number;
+    };
+  };
   responses: {
-    GetCity: components["responses"]["City"]
-    GetRoute: components["responses"]["Route"]
-    GetHealthCheck: components["responses"]["HealthCheck"]
-    ErrorResponse: components["responses"]["ErrorModel"]
+    GetCity: components["responses"]["City"];
+    GetRoute: components["responses"]["Route"];
+    GetHealthCheck: components["responses"]["HealthCheck"];
+    ErrorResponse: components["responses"]["ErrorModel"];
     /** @description An error returned when the requested resource cannot be found */
     NotFoundError: {
       content: {
@@ -77,73 +78,74 @@ export interface components {
           /** Not Found Error */
           data: {
             /** @enum {integer} */
-            status: 404
+            status: 404;
             /** @enum {string} */
-            message: "Not Found"
-          }
-        }
-      }
-    }
+            message: "Not Found";
+          };
+        };
+      };
+    };
     /** @description Error when there is a problem while fulfilling the request */
     ErrorModel: {
       content: {
         "application/json": {
-          data: components["schemas"]["ErrorModel"]
-        }
-      }
-    }
+          data: components["schemas"]["ErrorModel"];
+        };
+      };
+    };
     /** @description Returns city result */
     City: {
       content: {
         "application/json": {
-          data: components["schemas"]["City"]
-        }
-      }
-    }
+          data: components["schemas"]["City"];
+        };
+      };
+    };
     /** @description Returns route result */
     Route: {
       content: {
         "application/json": {
-          data: components["schemas"]["Route"]
-        }
-      }
-    }
+          data: components["schemas"]["Route"];
+        };
+      };
+    };
     /** @description Returns health check result */
     HealthCheck: {
       content: {
         "application/json": {
-          data: components["schemas"]["HealthCheckResult"]
-        }
-      }
-    }
-  }
+          data: components["schemas"]["HealthCheckResult"];
+        };
+      };
+    };
+  };
   parameters: {
     /**
      * @description City Identifier
      * @example paris
      */
-    PathCityID: string
+    PathCityID: string;
     /**
      * @description City Identifier
      * @example london
      */
-    QueryFrom: string
+    QueryFrom: string;
     /**
      * @description City Identifier
      * @example milan
      */
-    QueryTo: string
-  }
-  requestBodies: never
-  headers: never
-  pathItems: never
+    QueryTo: string;
+  };
+  requestBodies: never;
+  headers: never;
+  pathItems: never;
 }
 
-export type $defs = Record<string, never>
+export type $defs = Record<string, never>;
 
-export type external = Record<string, never>
+export type external = Record<string, never>;
 
 export interface operations {
+
   /**
    * Get City
    * @description Get a city given an ID
@@ -151,15 +153,15 @@ export interface operations {
   getCity: {
     parameters: {
       path: {
-        id: components["parameters"]["PathCityID"]
-      }
-    }
+        id: components["parameters"]["PathCityID"];
+      };
+    };
     responses: {
-      200: components["responses"]["City"]
-      404: components["responses"]["NotFoundError"]
-      default: components["responses"]["ErrorModel"]
-    }
-  }
+      200: components["responses"]["City"];
+      404: components["responses"]["NotFoundError"];
+      default: components["responses"]["ErrorModel"];
+    };
+  };
   /**
    * Get a Route
    * @description Get a route between two cities
@@ -167,26 +169,26 @@ export interface operations {
   getRoute: {
     parameters: {
       query: {
-        to: components["parameters"]["QueryFrom"]
-        from: components["parameters"]["QueryTo"]
-      }
-    }
+        to: components["parameters"]["QueryFrom"];
+        from: components["parameters"]["QueryTo"];
+      };
+    };
     responses: {
-      200: components["responses"]["Route"]
-      404: components["responses"]["NotFoundError"]
-      default: components["responses"]["ErrorModel"]
-    }
-  }
+      200: components["responses"]["Route"];
+      404: components["responses"]["NotFoundError"];
+      default: components["responses"]["ErrorModel"];
+    };
+  };
   /**
    * Get Health Check Status
    * @description Returns the health check for the service
    */
   getHealthCheck: {
     responses: {
-      200: components["responses"]["HealthCheck"]
+      200: components["responses"]["HealthCheck"];
       /** Unauthorized */
-      401: components["responses"]["ErrorModel"]
-      default: components["responses"]["ErrorModel"]
-    }
-  }
+      401: components["responses"]["ErrorModel"];
+      default: components["responses"]["ErrorModel"];
+    };
+  };
 }
